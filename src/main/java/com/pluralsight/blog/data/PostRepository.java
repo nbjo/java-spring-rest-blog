26a02b31630f17f62b4b11df0b920cf0824a664d
nijo dev <nxvdownload@gmail.com>
2022-06-27 23:25:51 +0200

    Completed module

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -9,21 +9,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@Component
-public class PostRepository {
-    public List<Post> findAll() {
-        return new ArrayList<>();
-    }
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
-    public List<Post> saveAll(List<Post> posts) {
-        return new ArrayList<>();
-    }
-
-    public Post save(Post post) {
-        return null;
-    }
-
-    public Optional<Post> findById(Long id) {
-        return null;
-    }
+public interface PostRepository extends JpaRepository<Post, Long> {
+    @RestResource(rel="contains-title", path="containsTitle")
+    public List<Post> findByTitleContaining(String title);
 }
